32afc8ec807c7859359b24e575636babcff1b8f2
Kevin Endara <kevinendara1997@gmail.com>
2025-05-15 14:18:22 -0500

    Cambios finales

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Sustituye ${PORT} en nginx.conf.template y lo guarda como nginx.conf
-envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
+# Sustituye el puerto y lo inserta dentro de una configuración válida
+envsubst '${PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
 
 # Ejecuta nginx en primer plano
 exec nginx -g 'daemon off;'
